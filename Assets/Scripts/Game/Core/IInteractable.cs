c605f7d1c24e91f42351bf47e937bd54373eda7b
Ramazan-future <iamassanramazan@gmail.com>
2025-10-03 15:09:49 +0500

    Первый коммит

---
+++ Assets/Scripts/Game/Core/IInteractable.cs
@@ -0,0 +1,5 @@
+public interface IInteractable {
+    string Prompt { get; }
+    void Interact(PlayerInteraction2D interactor);
+}
+
--- Assets/Scripts/MENUCONTROLLER/NEW GAME.cs
+++ Assets/Scripts/MENUCONTROLLER/NEW GAME.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;  
+using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    // --- Обычное переключение сцен ---
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -11,5 +12,19 @@ public class SceneSwitcher : MonoBehaviour
     public void ChangeScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    // --- Переход в Lobby после выхода из уровня ---
+    private static int nextLobbyIndex = 0; // какой лобби активировать
+
+    public static void ReturnToLobby(int lobbyIndex)
+    {
+        nextLobbyIndex = lobbyIndex;
+        SceneManager.LoadScene("Lobby"); // загружаем сцену Lobby
+    }
+
+    public static int GetNextLobbyIndex()
+    {
+        return nextLobbyIndex;
     }
 }
